71efca2036544778f7aec5c3c2516257aaeefd39
ChasingRain <junpeng94@gmail.com>
2022-12-22 13:12:29 +0800

    added create user capability

--- native-frontend/src/screens/LoginScreen.js
+++ native-frontend/src/screens/LoginScreen.js
@@ -1,12 +1,123 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase.config';
 
 function LoginScreen({navigation}) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = () => {
+    console.log(email, password)
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log("user created")
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Sign up err: ", errorMessage, errorCode)
+    });
+  }
+
   return (
-    <View>
-        <Text>Login Screen</Text>
+    <View style={styles.main}>
+        <KeyboardAwareScrollView>
+          <TextInput
+            style={styles.input}
+            placeholder='Email'
+            placeholderTextColor="#aaaaaa"
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+            underlineColorAndroid="transparent"
+            autoCapitalize="none"
+                  />
+          <TextInput
+              style={styles.input}
+              placeholderTextColor="#aaaaaa"
+              secureTextEntry
+              placeholder='Password'
+              onChangeText={(text) => setPassword(text)}
+              value={password}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+          />
+          <TouchableOpacity
+              style={styles.button}
+              onPress={handleLogin}>
+              <Text style={styles.buttonTitle}>Log in</Text>
+          </TouchableOpacity>
+
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>Register for an account</Text>
+        </View>
+        </KeyboardAwareScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  main: {
+    marginTop: 100
+  },
+  container: {
+      flex: 1,
+      alignItems: 'center'
+  },
+  title: {
+
+  },
+  logo: {
+      flex: 1,
+      height: 120,
+      width: 90,
+      alignSelf: "center",
+      margin: 30
+  },
+  input: {
+      height: 48,
+      borderRadius: 5,
+      overflow: 'hidden',
+      backgroundColor: 'white',
+      marginTop: 10,
+      marginBottom: 10,
+      marginLeft: 30,
+      marginRight: 30,
+      paddingLeft: 16
+  },
+  button: {
+      backgroundColor: '#788eec',
+      marginLeft: 30,
+      marginRight: 30,
+      marginTop: 20,
+      height: 48,
+      borderRadius: 5,
+      alignItems: "center",
+      justifyContent: 'center'
+  },
+  buttonTitle: {
+      color: 'white',
+      fontSize: 16,
+      fontWeight: "bold"
+  },
+  footerView: {
+      flex: 1,
+      alignItems: "center",
+      marginTop: 20
+  },
+  footerText: {
+      fontSize: 16,
+      color: '#2e2e2d'
+  },
+  footerLink: {
+      color: "#788eec",
+      fontWeight: "bold",
+      fontSize: 16
+  }
+})
 
 export default LoginScreen
